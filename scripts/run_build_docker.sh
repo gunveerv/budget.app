7c530b5c8f92c3db33d91da07c7714d4a539b074
Gunveer Vilkhu <gunveerv123@gmail.com>
2023-11-21 15:51:53 -0500

    script update

--- scripts/run_build_docker.sh
+++ scripts/run_build_docker.sh
@@ -16,11 +16,14 @@ fi
 docker build -t $IMAGE_NAME .
 
 # Start the application using Docker Compose
+
+# Raspberry Pi
 if [ $IS_RBP == "true" ]; then
     echo "... running RBP Docker compose"
     IP=$(hostname -I | cut -f1 -d' ')
     echo -e "\e[32m Access on local network: ${IP}:3000\e[0m"
     docker compose up
+# Other     
 else    
     echo "... running  non-RBP Docker-compose"
     docker-compose up
