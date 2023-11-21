ee99a567ffc140ce8746d6be3f01e517a2f8c38f
Gunveer Vilkhu <gunveerv123@gmail.com>
2023-11-21 17:15:51 -0500

    added some endpoint in index

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 
+// Debug Purposes
+const DEBUG_INDEX = process.env.DEBUG || false; 
+
 // Middleware
 dotenv.config({ path: './.env' })
 app.use(express.json());
@@ -9,8 +12,22 @@ app.use(express.json());
 // Simple endpoint route handler
 app.get('/', (req, res) => {
   res.send('Hello, this is a simple Express API for Budget.App!');
-  console.log(req.body);
+  if (DEBUG_INDEX) {
+    console.log(req.body);
+  }
 });
+
+// Status sample
+app.get("/status", (req, res) => {
+  const status = {
+    "Status": "Running",
+    "Message": "hello world"
+  };
+  if (DEBUG_INDEX) {
+    console.log(req.body);
+  }
+  res.send(status)
+}); 
 
 // Start the server
 const port = process.env.PORT || 3000;
